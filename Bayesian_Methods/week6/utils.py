6aad1663fc17a05d6593e0609d61d19e1656c084
ArtemPavlovich <kuuuubo@gmail.com>
2020-03-17 20:43:25 +0300

    update utils.py

--- Bayesian_Methods/week6/utils.py
+++ Bayesian_Methods/week6/utils.py
@@ -16,7 +16,7 @@ def create_encoder(input_dims, base_filters=64, layers=2, latent=512):
     for i in range(layers):
         encoder.add(Conv2D(filters=base_filters * 2**i, kernel_size=(5, 5),
                            strides=(2, 2), padding='same', use_bias=False, activation='relu'))
-        # encoder.add(BatchNormalization(axis=3))
+        encoder.add(BatchNormalization(axis=3))
     encoder.add(Reshape([w * h * c]))
     encoder.add(Dense(latent * 2))
     return encoder
@@ -33,7 +33,7 @@ def create_decoder(output_dims, base_filters=64, layers=2, latent=512):
     for i in range(layers - 1, 0, -1):
         decoder.add(Conv2DTranspose(filters=base_filters * 2**i, kernel_size=(5, 5),
                                     strides=(2, 2), padding='same', use_bias=False, activation='relu'))
-        # decoder.add(BatchNormalization(axis=3))
+        decoder.add(BatchNormalization(axis=3))
     decoder.add(Conv2DTranspose(filters=3, kernel_size=(5, 5),
                                 strides=(2, 2), padding='same', activation='sigmoid'))
     return decoder
@@ -58,6 +58,8 @@ def create_vae(batch_size, base_filters=128, latent=8,
     :param image_size: size of input image
     Returns compiled Keras model along with encoder and decoder
     """
+
+
     if isinstance(image_size, int):
         image_size = (image_size, image_size)
     x = Input(shape=(image_size[0], image_size[1], 3), batch_size=batch_size)
@@ -93,3 +95,30 @@ def create_vae(batch_size, base_filters=128, latent=8,
                 loss=loss)
 
     return vae, encoder, decoder
+
+
+weights_options = {1:
+    ['https://github.com/ArtemPavlovich/HSE_AML_course/raw/master/Bayesian_Methods/week6/VAE_celeba_img64_filt128_4layers_32lsize_3.h5',
+    'VAE_celeba_img64_filt128_4layers_32lsize_3.h5'],
+    2: ['https://github.com/ArtemPavlovich/HSE_AML_course/raw/master/Bayesian_Methods/week6/VAE_celeba_img64_filt256_4layers_16lsize_2.h5',
+    'VAE_celeba_img64_filt256_4layers_16lsize_2.h5']}
+
+
+def parse_name(weight_name):
+    split = weight_name.split('_')
+    image_size = int(split[2][3:])
+    base_filters = int(split[3][5:])
+    layers = int(split[4][:-6])
+    latent_size = int(split[5][:-5])
+    return image_size, base_filters, layers, latent_size
+
+
+def download_and_get_params(option=1):
+    url = weights_options[option][0]
+    filename = weights_options[option][1]
+
+    params = parse_name(filename)
+
+    !wget $url -O $filename
+
+    return params, filename
